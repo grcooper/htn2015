0c73ba874c09cb6bbda6c4d3ae34fc15996d9028
Shameel Abdullah <shameel.abdullah@gmail.com>
2015-09-19 12:26:00 -0400

    added correct files for physics and formatting

--- source/hellophaser/index.js
+++ source/hellophaser/index.js
@@ -56,67 +56,64 @@ window.onload = function() {
     function create() {
 
         //Physics
-        //game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.startSystem(Phaser.Physics.ARCADE);
 
         //Background
         game.add.sprite(0, 0, 'sky');
         characters = game.add.group();
-	//  Our controls.
-	cursor = game.input.keyboard.createCursorKeys();
+        //  Our controls.
+        cursor = game.input.keyboard.createCursorKeys();
     }
 
     function update() {
-	game.input.keyboard.onDownCallback = function(e) {
-	    
-	}
-	if(!start){
-	  game.input.keyboard.onDownCallback = function(e) {
-	  if(e.keyCode === 13){
-	    for (var i = 0; i < STARTORG; i++) {
-	      player = characters.create(game.world.width * Math.random(), game.world.height * Math.random(), 'dude');
-	      player.org = new createOrganism_A(getStrValue(), getIntValue(), getSpeedValue());
-	      player.org.timeout = 0;
-	    }
-	      start = true;
-	    }
-	  }
-	}
-	else{
-        characters.forEach(function(char) {
-            //loop through all characters
-            var xdirection;
-            var ydirection;
-            xdirection = Math.round(Math.random());
-            ydirection = Math.round(Math.random());
 
-            if (char.org.timeout == 30) {
-                char.org.timeout = 0;
-                if (xdirection) {
-                    char.body.velocity.x = char.org.speed * 20;
+        if (!start) {
+            game.input.keyboard.onDownCallback = function(e) {
+                if (e.keyCode === 13) {
+                    for (var i = 0; i < STARTORG; i++) {
+                        player = characters.create(game.world.width * Math.random(), game.world.height * Math.random(), 'dude');
+                        player.org = new createOrganism_A(getStrValue(), getIntValue(), getSpeedValue());
+                        player.org.timeout = 0;
+                    }
+                    start = true;
+                }
+            }
+        } else {
+            characters.forEach(function(char) {
+                //loop through all characters
+                var xdirection;
+                var ydirection;
+                xdirection = Math.round(Math.random());
+                ydirection = Math.round(Math.random());
+
+                if (char.org.timeout == 30) {
+                    char.org.timeout = 0;
+                    if (xdirection) {
+                        char.body.velocity.x = char.org.speed * 20;
+                    } else {
+                        char.body.velocity.x = char.org.speed * -20;
+                    }
+
+                    if (ydirection) {
+                        char.body.velocity.y = char.org.speed * 20;
+                    } else {
+                        char.body.velocity.y = char.org.speed * -20;
+                    }
                 } else {
+                    char.org.timeout += 1;
+                }
+
+                if (char.body.x >= (WIDTH - char.body.width)) {
                     char.body.velocity.x = char.org.speed * -20;
+                } else if (char.body.x <= 0) {
+                    char.body.velocity.x = char.org.speed * 20;
                 }
-
-                if (ydirection) {
-                    char.body.velocity.y = char.org.speed * 20;
-                } else {
+                if (char.body.y >= (HEIGHT - char.body.height)) {
                     char.body.velocity.y = char.org.speed * -20;
+                } else if (char.body.y <= 0) {
+                    char.body.velocity.y = char.org.speed * 20;
                 }
-            } else {
-                char.org.timeout += 1;
-            }
-
-            if (char.body.x >= (WIDTH - char.body.width)) {
-                char.body.velocity.x = char.org.speed * -20;
-            } else if (char.body.x <= 0) {
-                char.body.velocity.x = char.org.speed * 20;
-            }
-            if (char.body.y >= (HEIGHT - char.body.height)) {
-                char.body.velocity.y = char.org.speed * -20;
-            } else if (char.body.y <= 0) {
-                char.body.velocity.y = char.org.speed * 20;
-            }
-        });
-      }
+            });
+        }
     }
 }
