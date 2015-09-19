e57847c38de3f49b2021c711e64818ee5897f60f
thadeaus <graham.cooper33@gmail.com>
2015-09-19 11:13:20 -0400

    added collisions and multiple people

--- source/hellophaser/index.js
+++ source/hellophaser/index.js
@@ -22,10 +22,13 @@
         var organism = new Organism(6, 1, 4);
         return organism;
     }
-
+  
+    var WIDTH = 800;
+    var HEIGHT = 600;
+    var STARTORG = 5;
     window.onload = function() {
 
-        var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+        var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
         function preload () {
 
@@ -44,11 +47,10 @@
             game.add.sprite(0, 0, 'sky');
 
             characters = game.add.group();
-            player = characters.create(32, game.world.height - 150, 'dude');
-            player.org = new createOrganism_A();
-
-
-
+	    for(var i = 0; i < STARTORG; i++){
+	      player = characters.create(game.world.width * Math.random(), game.world.height * Math.random(), 'dude');
+	      player.org = new createOrganism_A();
+	    }
         }
 
         function update () {
@@ -59,7 +61,7 @@
                 var ydirection;
                 xdirection = Math.round(Math.random());
                 ydirection = Math.round(Math.random());
-
+		
                 if(char.org.timeout == 30){
                     char.org.timeout = 0;
                 if(xdirection){
@@ -79,7 +81,19 @@
                 else {
                     char.org.timeout += 1;
                 }
-
+		
+		if(char.body.x >= (WIDTH-char.body.width)){
+		  char.body.velocity.x = char.org.speed * -20;
+		}
+		else if(char.body.x <= 0){
+		  char.body.velocity.x = char.org.speed * 20;
+		}
+		if(char.body.y >= (HEIGHT-char.body.height)){
+		  char.body.velocity.y = char.org.speed * -20;
+		}
+		else if(char.body.y <= 0){
+		  char.body.velocity.y = char.org.speed * 20;
+		}
             });
 
         }
