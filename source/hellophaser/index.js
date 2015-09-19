057c8988e6ab2a4a034bc304cc92dbbc374a53b6
Shameel Abdullah <shameel.abdullah@gmail.com>
2015-09-19 16:45:07 -0400

    yummy physics

--- source/hellophaser/index.js
+++ source/hellophaser/index.js
@@ -73,6 +73,7 @@ window.onload = function() {
         game.add.sprite(0, 0, 'sky');
         characters = game.add.group();
         characters.enableBody = true;
+        yummy.enableBody = true;
         //  Our controls.
         cursor = game.input.keyboard.createCursorKeys();
 
@@ -136,7 +137,7 @@ window.onload = function() {
             e1.strength -= 2;
             e2.strength -= 2;
 
-	   return organism;
+            return organism;
         }
     }
 
@@ -147,7 +148,7 @@ window.onload = function() {
 
     function update() {
 
-        game.physics.arcade.collide(characters,characters, collisionHandler, null, this);
+        game.physics.arcade.collide(characters, characters, collisionHandler, null, this);
         game.physics.arcade.collide(characters, yummy, eatsMeat, null, this);
 
         if (start) {
@@ -160,10 +161,10 @@ window.onload = function() {
 
                 if (char.org.timeout == 30) {
                     char.org.timeout = 0;
-		    var dir = chooseDir(char);
-		    console.log(dir);
+                    var dir = chooseDir(char);
+                    console.log(dir);
                     char.body.velocity.x = char.org.speed * dir.x;
-		    char.body.velocity.y = char.org.speed * dir.y;
+                    char.body.velocity.y = char.org.speed * dir.y;
                 } else {
                     char.org.timeout += 1;
                 }
@@ -181,51 +182,52 @@ window.onload = function() {
             });
         }
     }
-function chooseDir(currentCell) {
-    var intel = currentCell.org.intelligence;
-    var str = currentCell.org.strength;
-    var x = currentCell.body.x;
-    var y = currentCell.body.y;
-    var range = intel * 30; // 30 is arbitrary
-    var smartness = intel * Math.random();
 
-    var dir = {x: 0, y: 0};
+    function chooseDir(currentCell) {
+        var intel = currentCell.org.intelligence;
+        var str = currentCell.org.strength;
+        var x = currentCell.body.x;
+        var y = currentCell.body.y;
+        var range = intel * 30; // 30 is arbitrary
+        var smartness = intel * Math.random();
 
-    if (smartness > 0.2) {
-	console.log("hi");
-        var len = characters.length;
-        var found = false;
-        for (var i = 0; i < len; i++) {
-            var ex = characters.children[i].x;
-            var ey = characters.children[i].y;
-            if(currentCell.org.name != characters.children[i].org.name){
-	    if (Math.abs(x - ex) < range && Math.abs(y - ey) < range) {
-                var estr = characters.children[i].org.strength;
-                if (estr > str) {
-		    var len = Math.sqrt((x * x) + (y * y));
-                    dir.x = (ex - x)/len;
-		    dir.y = (ey - y)/len;
+        var dir = {
+            x: 0,
+            y: 0
+        };
+
+        if (smartness > 0.2) {
+            console.log("hi");
+            var len = characters.length;
+            var found = false;
+            for (var i = 0; i < len; i++) {
+                var ex = characters.children[i].x;
+                var ey = characters.children[i].y;
+                if (currentCell.org.name != characters.children[i].org.name) {
+                    if (Math.abs(x - ex) < range && Math.abs(y - ey) < range) {
+                        var estr = characters.children[i].org.strength;
+                        if (estr > str) {
+                            var len = Math.sqrt((x * x) + (y * y));
+                            dir.x = (ex - x) / len;
+                            dir.y = (ey - y) / len;
+                        } else {
+                            var len = Math.sqrt((x * x) + (y * y));
+                            dir.x = -(ex - x) / len;
+                            dir.y = -(ey - y) / len;
+                        }
+                        found = true;
+                        break;
+                    }
                 }
-		else {
-		    var len = Math.sqrt((x * x) + (y * y));
-		    dir.x = -(ex - x)/len;
-		    dir.y = -(ey - y)/len;
-		}
-		found = true;
-		break;
             }
-	    }
         }
+        if (found == false) {
+            dir.x = Math.random();
+            dir.y = Math.random();
+            var len = Math.sqrt((x * x) + (y * y));
+            dir.x = dir.x / len;
+            dir.y = dir.y / len;
+        }
+        return dir;
     }
-    if(found == false){
-      dir.x = Math.random();
-      dir.y = Math.random();
-      var len = Math.sqrt((x * x) + (y * y));
-      dir.x = dir.x/len;
-      dir.y = dir.y/len;
-    }
-    return dir;
 }
-}
-
-
